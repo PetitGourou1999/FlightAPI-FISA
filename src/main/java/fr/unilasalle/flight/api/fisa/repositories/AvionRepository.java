cb274a440f18126c35f50a06c51fcba6ad372c96
Meg-Ann Camus <meg-ann.camus@outlook.fr>
2023-11-21 17:26:07 +0200

    Fin du TD

---
+++ src/main/java/fr/unilasalle/flight/api/fisa/repositories/AvionRepository.java
@@ -0,0 +1,15 @@
+package fr.unilasalle.flight.api.fisa.repositories;
+
+import fr.unilasalle.flight.api.fisa.beans.Avion;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.inject.Model;
+
+import java.util.List;
+
+@Model
+public class AvionRepository implements PanacheRepositoryBase<Avion, Long> {
+
+    public List<Avion> findByOperator(String operatorParameter){
+        return find("operator", operatorParameter).list();
+    }
+}
